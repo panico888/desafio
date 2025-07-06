4001c50609902b72f8c71f3a0ff70a0883117dba
prosty <benhurcasemirodias@gmail.com>
2025-07-06 15:35:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('ola mundo');
+// nivel do heroi
+
+let nome = 'Pedro';
+let Expdoheroi = 3000;
+
+for (let xp = 0; xp <= 9000; xp += 1000) {
+    let nivel = '';
+
+    if (xp <= 1000) {
+        nivel = 'Ferro'; 
+    } else if (xp <= 2000) {
+        nivel = 'Bronze';
+    } else if (xp <= 3000) {
+        nivel = 'Prata';
+    } else if (xp <= 5000) {
+        nivel = 'Ouro';
+    } else if (xp <= 8000) {
+        nivel = 'Platina';
+    } else {
+        nivel = 'Máximo';
+    }
+
+    if (xp === Expdoheroi) {
+        console.log(`${nome} está no nível ${nivel}`);
+    } else {
+        console.log(`Nível ${nivel}`);
+    }
+}
